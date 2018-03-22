7a807321aad75dc1caaaa21f221fec10494f999b
Scott Yantek <scott.yantek@gmail.com>
2018-03-22 11:56:01 -0400

    more work on attack implementation.
    enable on/off switch parameter and basic functions of the attack class and app are implemented

--- src/modules/attack/attack_params.c
+++ src/modules/attack/attack_params.c
@@ -52,8 +52,30 @@
  * performed or not. It allows for all desired attack types to be switched on
  * at once, so that different attack types can be combined and parameters can 
  * be adjusted without the attack starting until this switch is turned on.
+ * The switch is on when its value is 1478 and off for all other values.
  *
- * @boolean 
+ * @boolean
  * @group Attack
  */
-PARAM_DEFINE_BOOL(ATTACK_MASTER_SWITCH, 0);
+PARAM_DEFINE_INT32(ATTACK_MSTR_SW, 0);
+
+/**
+ * Attack type
+ *
+ * Integer bitmask for setting the attack type. The value of this parameter 
+ * corresponds to the type of attack that will be carried out when 
+ * ATTACK_MSTR_SW is on. Generally, this should be set up before turning on the 
+ * switch. Set bits to 1 to enable the attack type. Some of the attacks can be
+ * mixed, but it does not make any practical sense to mix stealthy attacks with
+ * other types of attack. TODO add more attack types as they are implemented
+ *
+ * 0 : pitch oscillation
+ * 1 : stealthy to human observer (alter estimator position output)
+ *
+ * @group Attack
+ * @min 0
+ * @max 3
+ * @bit 0 enable pitch oscillation attack
+ * @bit 1 enable stealthy-to-human-observer-model attack
+ */
+PARAM_DEFINE_INT32(ATTACK_TYPE, 0);
